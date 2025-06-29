c0241d13fd2c8cf0108fb1d309871e4dfa132c6a
Elias Benkhodja <elias.benkhodja@gmail.com>
2025-06-29 23:48:15 +0300

    Music wheel scores & tech notation & caching (#4)
    * [proto] Display tech in musicwheelitem
    
    (cherry picked from commit c47f01d7d3a674df48613c0e493be53c776e5ab1)
    
    * [proto] display ex score in song wheel
    
    Unify EX score calculations
    
    Also hide Lamp when showing EX
    
    (cherry picked from commit 9fecf6f377318c76597af79ba648e4a69337064a)
    
    * Add verbose / condensed modes for tech notation
    
    (cherry picked from commit e900d5cf8de1bee9ddad6c350dde76f42b1b99bc)
    
    * Clarify descriptions of new options
    
    (cherry picked from commit 4687806d5973ba0c43d279565c1ee5a00df1e9b6)
    
    * Decouple tech string generation from UI
    
    String formatting logic is now in a separate function,
    `SLTechNotation_Format`. This file / function can be merged or
    integrated separately from other changes.
    
    (cherry picked from commit ca1112830bb545ad8d101f64c2d23f1c16f58752)
    
    * Tech notation to density graph / tech breakdown
    
    - Move total stream percentage to be displayed after peak NPS, it's
      logically more bound to other stream statistics than tech.
    
    - Display doublesteps in the additional info / tech breakdown section on
      the old place of stream percentage.
    
    - Display auto generated tech notation string above the tech breakdown
      section similarly to peak NPS.
    
    (cherry picked from commit 954eb3dbee96a53447d7782463e7339729ca8fb3)
    
    * Cache GrooveStats EX scores locally
    
    The caches scores are displayed in MusicWheelItem when the SL setting
    for music wheel scores is enabled.
    
    TODO:
     - Cache ITG scores as well, depending on which setting the player has
       enabled
    (cherry picked from commit c202c40178ec8ab6fe393e06012c9939890ad2d6)
    
    * Change DB table schema to support arbitrary score types and sources
    
    Add support for local & GrooveStats ITG scores.
    Improve Lua in-memory caching.
    
    (cherry picked from commit e41eeb984d23d52c175085cf3709c6afb0a48c60)
    
    * More MusicWheelScore options
    
    (cherry picked from commit b0121003d6d79e16d761d60109c5c3f006073bd6)
    
    * Rename EXScore -> Score
    
    (cherry picked from commit 5b010052f113c30004ce9ad8a9f1543c4d0e5dd4)
    
    * Tech notation positioning fixes
    
    * Set x offset depending on the value of MusicWheelScore
    * Add missing "-" to light sideswitch notation
    
    (cherry picked from commit 6d91ac56a3ad5b7983a3402ffde78d21c380305e)
    
    * Music Wheel EX Score -> Music Wheel Score in SL options
    
    ---------
    
    Co-authored-by: vide <git@vide.foo>

--- Scripts/SL_ITL.lua
+++ Scripts/SL_ITL.lua
@@ -1,12 +1,10 @@
 -- -----------------------------------------------------------------------
-IsItlSong = function(player)
-	local song = GAMESTATE:GetCurrentSong()
+IsItlSong = function(song, player)
 	local song_dir = song:GetSongDir()
 	local group = string.lower(song:GetGroupName())
 	local pn = ToEnumShortString(player)
 	return string.find(group, "itl online 2026") or string.find(group, "itl 2026") or SL[pn].ITLData["pathMap"][song_dir] ~= nil
 end
-
 
 IsItlActive = function()
 	-- The file is only written to while the event is active.
@@ -72,7 +70,7 @@ WriteItlFile = function(player)
 		[PLAYER_1] = "ProfileSlot_Player1",
 		[PLAYER_2] = "ProfileSlot_Player2"
 	}
-	
+
 	local dir = PROFILEMAN:GetProfileDir(profile_slot[player])
 	-- We require an explicit profile to be loaded.
 	if not dir or #dir == 0 then return end
@@ -94,14 +92,14 @@ ReadItlFile = function(player)
 		[PLAYER_1] = "ProfileSlot_Player1",
 		[PLAYER_2] = "ProfileSlot_Player2"
 	}
-	
+
 	local dir = PROFILEMAN:GetProfileDir(profile_slot[player])
 	local pn = ToEnumShortString(player)
 	-- We require an explicit profile to be loaded.
 	if not dir or #dir == 0 then return end
 
 	local path = dir .. itlFilePath
-	local itlData = { 
+	local itlData = {
 		["pathMap"] = {},
 		["hashMap"] = {},
 		["unlockFolders"] = {},
@@ -248,7 +246,7 @@ local DataForSong = function(player, prevData)
 			noCmod = prevData["noCmod"]
 		end
 	end
-	
+
 	local year = Year()
 	local month = MonthOfYear()+1
 	local day = DayOfMonth()
@@ -259,7 +257,7 @@ local DataForSong = function(player, prevData)
 	local points = GetITLPointsForSong(passingPoints, maxScoringPoints, ex)
 	local usedCmod = GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Preferred"):CMod() ~= nil
 	local date = ("%04d-%02d-%02d"):format(year, month, day)
-	
+
 	return {
 		["judgments"] = judgments,
 		["ex"] = ex * 100,
@@ -280,7 +278,7 @@ end
 UpdateItlData = function(player)
 	local pn = ToEnumShortString(player)
 	local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
-		
+
 	-- Do the same validation as GrooveStats.
 	-- This checks important things like timing windows, addition/removal of arrows, etc.
 	local _, valid, _ = ValidForGrooveStats(player)
@@ -327,7 +325,7 @@ UpdateItlData = function(player)
 			local pathMap = SL[pn].ITLData["pathMap"]
 			pathMap[song_dir] = hash
 		end
-		
+
 		-- Then maybe update the hashMap.
 		local updated = false
 		if hashMap[hash] == nil then
@@ -349,7 +347,7 @@ UpdateItlData = function(player)
 			if data["ex"] >= hashMap[hash]["ex"] then
 				hashMap[hash]["ex"] = data["ex"]
 				-- hashMap[hash]["points"] = data["points"]
-				
+
 				if data["ex"] > hashMap[hash]["ex"] then
 					-- EX count is strictly better, copy the judgments over.
 					hashMap[hash]["judgments"] = DeepCopy(data["judgments"])
@@ -375,7 +373,7 @@ UpdateItlData = function(player)
 						updated = true
 					end
 				end
-			end	
+			end
 
 			if data["clearType"] > hashMap[hash]["clearType"] then
 				hashMap[hash]["clearType"] = data["clearType"]
