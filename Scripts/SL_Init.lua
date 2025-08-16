0559a57e5989b66b0a3c97908689f90ba6445da5
Arttu Ylä-Outinen <aryla@users.noreply.github.com>
2025-08-16 14:32:04 +0300

    Use chart info from engine
    Change ParseChartInfo to use the tech counts, density data, and column
    cues from the engine instead of parsing the chart. Reduces lag when
    selecting a different song.
    
    Adjusts the histogram and column cues to account for differences between
    the formats: ColumnCues needs to check `noteType` instead of `isMine`,
    and the histogram needs use the correct time signatures. Theme NPS
    calculation always assumed a 4/4 signature.

--- Scripts/SL_Init.lua
+++ Scripts/SL_Init.lua
@@ -74,7 +74,6 @@ local PlayerDefaults = {
 
 				-- Information parsed out from the chart.
 				NotesPerMeasure = {},
-				EquallySpacedPerMeasure = {},
 				PeakNPS = 0,
 				NPSperMeasure = {},
 				columnCues = {},
