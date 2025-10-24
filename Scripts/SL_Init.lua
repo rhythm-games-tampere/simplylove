0e685a3186c686fb1de8db7abedb0521b6df7800
vide <git@vide.foo>
2025-10-24 15:10:58 +0300

    PlayerMusicWheelScore finalization
    * Add a default value in SL-Init ("No")
    * Add name / type to permitted_profile_settings table to permit saving / loading to Simply Love UserPrefs.ini
    * Rename "ReplaceGrade" to "Replace Grade" as spaces are ok in values

--- Scripts/SL_Init.lua
+++ Scripts/SL_Init.lua
@@ -60,6 +60,8 @@ local PlayerDefaults = {
 
 				NoteFieldOffsetX = 0,
 				NoteFieldOffsetY = 0,
+
+				PlayerMusicWheelScore = PlayerMusicWheelScore_No
 			}
 			-- TODO(teejusb): Rename "Streams" as the data contains more information than that.
 			self.Streams = {
