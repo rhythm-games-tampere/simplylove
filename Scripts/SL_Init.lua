cdb45083622d42d44277c9c05e6fd40b842257df
vide <git@vide.foo>
2026-09-26 11:31:33 +0300

    sm26 tarkkuus

--- Scripts/SL_Init.lua
+++ Scripts/SL_Init.lua
@@ -319,7 +319,7 @@ SL = {
 			AllowW1="AllowW1_Everywhere",
 			SubSortByNumSteps=true,
 
-			TimingWindowSecondsW1=0.013500,
+			TimingWindowSecondsW1=0.008500,
 			TimingWindowSecondsW2=0.021500,
 			TimingWindowSecondsW3=0.043000,
 			TimingWindowSecondsW4=0.102000,
